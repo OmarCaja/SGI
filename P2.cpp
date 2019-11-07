3d56a53ea7c4b699eceb14fda6ebcf454047e6c2
Omar Caja <omar.caja@hotmail.com>
2019-11-07 23:35:29 +0100

    Modified logic of drawing using draw list and translations

--- P2.cpp
+++ P2.cpp
@@ -7,24 +7,48 @@
 #define RADIO_INT 1.0
 #define RADIO_EXT 0.7
 
-void printTriangle(double initialAngle)
+static GLuint triangle;
+
+void drawTriangle()
 {
-    glBegin(GL_TRIANGLE_STRIP);
-    glColor3f(0.0, 0.0, 0.3);
+    double initialAngle = PI / 6.0;
     double phaseAngle = 2 * PI / 3.0;
     for (int i = 0; i < 4; i++)
     {
         glVertex3f(RADIO_INT * cos(initialAngle + i * phaseAngle), RADIO_INT * sin(initialAngle + i * phaseAngle), Z_COORDINATE);
         glVertex3f(RADIO_EXT * cos(initialAngle + i * phaseAngle), RADIO_EXT * sin(initialAngle + i * phaseAngle), Z_COORDINATE);
     }
+}
+
+void init()
+{
+    triangle = glGenLists(1);
+
+    glNewList(triangle, GL_COMPILE);
+
+    glBegin(GL_TRIANGLE_STRIP);
+    drawTriangle();
     glEnd();
+
+    glEndList();
+}
+
+void drawStart()
+{
+    glColor3f(0.0, 0.0, 0.3);
+    glCallList(triangle);
+
+    glRotatef(180, 0, 0, 1);
+    glCallList(triangle);
+    glPushMatrix();
 }
 
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    printTriangle(PI / 6.0);
-    printTriangle(PI / 2.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    drawStart();
     glFlush();
 }
 
@@ -35,6 +59,7 @@ int main(int argc, char **argv)
     glutInitWindowSize(400, 400);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("SGI P2");
+    init();
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
